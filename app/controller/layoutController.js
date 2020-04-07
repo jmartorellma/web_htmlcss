e4d61e656fdc8a15ead81088f143c19c38107824
jmartorellma <jmartorell@uoc.edu>
2020-04-07 21:00:49 +0200

    Navegació responsive

--- app/controller/layoutController.js
+++ app/controller/layoutController.js
@@ -1,23 +1,39 @@
 const SetLayoutController = () => {
     angular.module('MainApp')
-    .controller('LayoutController', ['$scope', '$state', 'CATEGORIES',
-        ($scope, $state, CATEGORIES) => {
+    .controller('LayoutController', ['$scope', '$state', '$mdSidenav', 'CATEGORIES',
+        ($scope, $state, $mdSidenav, CATEGORIES) => {
 
             $scope.categoriesList = Object.entries(CATEGORIES);
 
-            $scope.showMenu = (menuCategoria) => {
-                const listId = '#' + menuCategoria.buttonId;
+            $scope.showCategoria = (menuCategoria) => {
+                const idButton = '#' + menuCategoria.buttonId;
                 const idList = '#' + menuCategoria.listId;
-                const button = document.querySelector(listId);
+                const button = document.querySelector(idButton);
                 const list = document.querySelector(idList);
                 list.style.width = button.offsetWidth.toString()+"px";
                 menuCategoria.show = true;
             };
-            $scope.goCategoria = (param) => {
+
+            $scope.toggleMenu = (idSideNav) => {
+                const side = $mdSidenav(idSideNav); 
+                side.open();
+            }
+
+            $scope.goCategoria = (param, menuCategoria) => {
+                menuCategoria.show = false;
+                if($mdSidenav('sideNavMenuBurguer') != undefined && 
+                   $mdSidenav('sideNavMenuBurguer').isOpen()){
+                    $mdSidenav('sideNavMenuBurguer').close();
+                   }
                 $state.go('Categoria', { typeCategoria: param });
             }
 
-            $scope.goDetall = (paramCategoria, paramDetall) =>{
+            $scope.goDetall = (paramCategoria, menuCategoria, paramDetall) =>{
+                menuCategoria.show = false;
+                if($mdSidenav('sideNavMenuBurguer') != undefined && 
+                   $mdSidenav('sideNavMenuBurguer').isOpen()){
+                    $mdSidenav('sideNavMenuBurguer').close();
+                   }
                 $state.go('Detall', { typeCat: paramCategoria, typeDetall: paramDetall });
             }
         }
