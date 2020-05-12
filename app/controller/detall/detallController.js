d1cd83104ecde18885ab366e8b1d323895b744d8
jmartorellma <jmartorell@uoc.edu>
2020-05-12 18:07:11 +0200

    images links detall

--- app/controller/detall/detallController.js
+++ app/controller/detall/detallController.js
@@ -8,7 +8,7 @@ const SetDetallController =  (images) => {
                     const constantDetall = CATEGORIES[$stateParams.typeCat].items.find((item)=>{return item.codi===$stateParams.typeDetall});
                     $scope.dataDetall.header = constantDetall ? constantDetall.desc : '';
                     $scope.dataDetall.imgLink = images[$stateParams.typeDetall];
-                    $scope.dataDetall.linksRe = CATEGORIES[$stateParams.typeCat].items.filter((item)=>{return item.codi !== $stateParams.typeDetall});
+                    $scope.dataDetall.linksRe = CATEGORIES[$stateParams.typeCat].items.filter((item)=>{return item.codi !== $stateParams.typeDetall}).map(i=> { return {'codi': i.codi, 'desc': i.desc, 'img': images[i.codi]}});
                 },
                 (error)=>{
                     console.log(error);
